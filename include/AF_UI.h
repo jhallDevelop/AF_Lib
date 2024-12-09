91c197a43bc8f34f6f7c4c106d76a306a63226ea
jhall.develop <jhall.develop@gmail.com>
2024-12-10 08:05:47 +1000

    EDIT: minor change to return type for loading font.

--- include/AF_UI.h
+++ include/AF_UI.h
@@ -17,4 +17,4 @@ void AF_UI_RendererSprite_Update(AF_CSprite* _sprite, AF_Time* _time);
 void AF_UI_RendererSprite_Play(AF_CSprite* _sprite, BOOL _isLooping);
 void AF_UI_Renderer_Finish(void);
 void AF_UI_Renderer_Shutdown(void);
-void AF_LoadFont(int _id, const char* _path, uint8_t _color[4]);
+void* AF_LoadFont(int _id, const char* _path, uint8_t _color[4]);
