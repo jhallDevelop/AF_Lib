9399b85a70cd3324f53d2d465e395e604458c1ae
jhall.develop <jhall.develop@gmail.com>
2025-04-29 21:49:12 +1000

    EDIT: definitions for helper functions

--- include/AF_Shader.h
+++ include/AF_Shader.h
@@ -33,7 +33,7 @@ Init shader struct
 */
 inline static AF_Shader AF_Shader_ZERO(void) {
     AF_Shader zeroShader = {
-        .shaderID = 0,
+        .shaderID = 0
     };
     
     zeroShader.fragPath[0] = '\0'; // Proper null-terminated empty string
@@ -58,7 +58,22 @@ REturn the shader ID or return -1 if failed
 ====================
 */
 uint32_t AF_Shader_Load(const char* _vertexShaderPath, const char* _fragmentShaderPath);
+
 void AF_Shader_Delete(uint32_t programID);
+
+
+// Helper Functions
+void AF_Shader_SetBool(uint32_t ID, const char* name, bool value);
+void AF_Shader_SetInt(uint32_t ID, const  char* name, int value);
+void AF_Shader_SetFloat(uint32_t ID, const char* name, float value);
+//void AF_Shader_SetVec2(uint32_t ID, const  char* name, const Vec2 value);
+void AF_Shader_SetVec2(uint32_t ID, const  char* name, float x, float y);
+//void AF_Shader_SetVec3(uint32_t ID, const  char* name, const Vec3 value);
+void AF_Shader_SetVec3(uint32_t ID, const  char* name, float x, float y, float z);
+//void AF_Shader_SetVec4(uint32_t ID, const char* name, const Vec4 value);
+void AF_Shader_SetVec4(uint32_t ID, const  char* name, float x, float y, float z, float w);
+void AF_Shader_SetMat4(uint32_t ID, const char* name, const Mat4 mat);
+
 
 #ifdef __cplusplus
 }
