5dbf71e5816faf2092287ee03a47c1c8021ac6a6
jhall.develop <jhall.develop@gmail.com>
2025-06-11 08:24:10 +1000

    EDIT: fix for use on osx and linux using declspec

--- include/AF_Log.h
+++ include/AF_Log.h
@@ -6,10 +6,17 @@ Definition of logging helper functions for the game
 Calls vfprintf but adds some colour to text output
 ===============================================================================
 */
-#ifdef AF_LIB_EXPORTS
-#define AF_LIB_API __declspec(dllexport)
+
+#if defined(_WIN32) || defined(_WIN64)
+    // This part is for Windows platforms (MSVC compiler)
+    #ifdef AF_LIB_EXPORTS // This macro should be defined by the library project itself
+        #define AF_LIB_API __declspec(dllexport)
+    #else
+        #define AF_LIB_API __declspec(dllimport)
+    #endif
 #else
-#define AF_LIB_API __declspec(dllimport)
+    // This part is for non-Windows platforms (GCC, Clang on macOS, Linux)
+    #define AF_LIB_API __attribute__((visibility("default")))
 #endif
 
 
