5d453b7aea99a0877d48cc1b70500d8d1864ddff
jhall.develop@gmail.com <jhall.develop@gmail.com>
2026-04-08 20:57:51 +1000

    EDIT: added export AF_LIB_API to help with windows linking step

--- include/ECS/Components/AF_Component.h
+++ include/ECS/Components/AF_Component.h
@@ -15,10 +15,10 @@ Definition for helper functions for components
 extern "C" {
 #endif
 
-PACKED_CHAR AF_Component_SetHas(AF_Flag8_t _component, af_bool_t _hasFlag);
-PACKED_CHAR AF_Component_SetEnabled(PACKED_CHAR _component, af_bool_t _enabledFlag);
-af_bool_t AF_Component_GetEnabled(AF_Flag8_t _flags);
-af_bool_t AF_Component_GetHasEnabled(AF_Flag8_t _flags);
+AF_LIB_API PACKED_CHAR AF_Component_SetHas(AF_Flag8_t _component, af_bool_t _hasFlag);
+AF_LIB_API PACKED_CHAR AF_Component_SetEnabled(PACKED_CHAR _component, af_bool_t _enabledFlag);
+AF_LIB_API af_bool_t AF_Component_GetEnabled(AF_Flag8_t _flags);
+AF_LIB_API af_bool_t AF_Component_GetHasEnabled(AF_Flag8_t _flags);
 AF_LIB_API af_bool_t AF_Component_GetHas(AF_Flag8_t _flags);
 
 #ifdef __cplusplus
